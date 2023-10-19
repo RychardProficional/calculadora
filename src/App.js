11466093ac9766389b7e057ad71938be0931a444
RychardProficional <rychardproficional@gmail.com>
2023-10-18 23:39:31 -0400

    configurando formatores de texto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import {ThemeProvider} from "styled-components";
-import {theme} from "./theme";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme.lite}>
-      <div className="App">
-        Hello World!!!
-      </div>
+      <div className="App">Hello World!!!</div>
     </ThemeProvider>
   );
 }
