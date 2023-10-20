b5fdbe6dd3ed4386f5b2aaa3a1ef82284966ec4a
RychardProficional <rychardproficional@gmail.com>
2023-10-19 22:16:04 -0400

    update ´calculator´

--- src/components/calculator/index.js
+++ src/components/calculator/index.js
@@ -12,7 +12,34 @@ const Calculator = () => {
           </tbody>
           <tr>
             <td>
+              <Button>(</Button>
+              <Button>)</Button>
+              <Button>%</Button>
+              <Button>AC</Button>
+            </td>
+            <td>
+              <Button>7</Button>
+              <Button>8</Button>
+              <Button>9</Button>
+              <Button>/</Button>
+            </td>
+            <td>
+              <Button>4</Button>
+              <Button>5</Button>
+              <Button>6</Button>
+              <Button>*</Button>
+            </td>
+            <td>
               <Button>1</Button>
+              <Button>2</Button>
+              <Button>3</Button>
+              <Button>-</Button>
+            </td>
+            <td>
+              <Button>0</Button>
+              <Button>.</Button>
+              <Button>=</Button>
+              <Button>+</Button>
             </td>
           </tr>
         </table>
