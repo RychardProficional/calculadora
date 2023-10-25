977f36da80c995efc780723a80651555d16a55dd
RychardProficional <rychardproficional@gmail.com>
2023-10-25 05:20:51 -0400

    update 'lite' theme

---
+++ src/theme.js
@@ -0,0 +1,52 @@
+export const theme = {
+  lite: {
+    name: "lite",
+    colors: {
+      primary: "#E4F6FF",
+      primaryLetter: "#010021",
+      primaryBackground: "#F0F1FF",
+
+      secondary: "#3071F2",
+      secondaryLetter: "#ECECF9",
+      secondaryBackground: "#D5CFE8",
+
+      btn: {
+        Equal: {
+          primary: "#B3C8F2",
+          secundary: "Corpo: #91B2F2",
+          Border: "#6B98F2",
+          Letter: "#010021",
+        },
+        Emphasis: {
+          primary: "#F0F1FF",
+          secundary: "#D5CFE8",
+          Border: "#CFD9E8",
+          Letter: "#010021",
+        },
+        Normal: {
+          primary: "#4B83F2",
+          secundary: "#3071F2",
+          Border: "#91B2F2",
+          Letter: "#F2CB05",
+        },
+      },
+    },
+
+    fonts: {
+      primaryText: "'Roboto Mono', monospace",
+      secondaryText: "Arial, Helvetica, sans-serif",
+
+      primaryNumber: "'Noto Sans', sans-serif",
+      secondaryNumber: "'Roboto Mono', monospace",
+
+      primaryTitle: "'Times New Roman', Times, serif",
+      secondaryTitle: "'Open Sans', sans-serif",
+    },
+  },
+
+  dark: {
+    name: "dark",
+    colors: {},
+    fonts: {},
+  },
+}
