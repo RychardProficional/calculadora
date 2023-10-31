1c53966a9f2d43ba92d05c092127322942895449
RychardProficional <rychardproficional@gmail.com>
2023-10-31 03:00:21 -0400

    increment colors theme ´dark´

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,14 @@
+export const fonts = {
+  primaryText: "'Roboto Mono', monospace",
+  secondaryText: "Arial, Helvetica, sans-serif",
+
+  primaryNumber: "'Noto Sans', sans-serif",
+  secondaryNumber: "'Roboto Mono', monospace",
+
+  primaryTitle: "'Times New Roman', Times, serif",
+  secondaryTitle: "'Open Sans', sans-serif",
+}
+
 export const theme = {
   lite: {
     name: "lite",
@@ -13,7 +24,7 @@ export const theme = {
       btn: {
         Equal: {
           primary: "#B3C8F2",
-          secundary: "Corpo: #91B2F2",
+          secundary: "#91B2F2",
           Border: "#6B98F2",
           Letter: "#010021",
         },
@@ -31,22 +42,41 @@ export const theme = {
         },
       },
     },
-
-    fonts: {
-      primaryText: "'Roboto Mono', monospace",
-      secondaryText: "Arial, Helvetica, sans-serif",
-
-      primaryNumber: "'Noto Sans', sans-serif",
-      secondaryNumber: "'Roboto Mono', monospace",
-
-      primaryTitle: "'Times New Roman', Times, serif",
-      secondaryTitle: "'Open Sans', sans-serif",
-    },
+    fonts: fonts,
   },
 
   dark: {
     name: "dark",
-    colors: {},
-    fonts: {},
+    colors: {
+      primary: "#023059",
+      primaryLetter: "#ececf9",
+      primaryBackground: "?",
+
+      secondary: "#08090D",
+      secondaryLetter: "#010021",
+      secondaryBackground: "?",
+
+      btn: {
+        Equal: {
+          primary: "#1A1E26",
+          secundary: "#08090D",
+          Border: "#343A40",
+          Letter: "#ECECF9",
+        },
+        Emphasis: {
+          primary: "#6E7371",
+          secundary: "#343A40",
+          Border: "#A1A69C",
+          Letter: "#F29F05",
+        },
+        Normal: {
+          primary: "#034C8C",
+          secundary: "#3071F2",
+          Border: "#023E73",
+          Letter: "#023059",
+        },
+        fonts: fonts,
+      },
+    },
   },
 }
